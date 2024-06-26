1710228ddefa9b792a12927b05c220e79f3b2f64
OD21IO <teofil.melinte@ing.com>
2024-06-26 12:08:54 +0300

    encrypted properties

--- src/main/java/upb/teo/e_commerce/ECommerceApplication.java
+++ src/main/java/upb/teo/e_commerce/ECommerceApplication.java
@@ -1,15 +1,14 @@
 package upb.teo.e_commerce;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEncryptableProperties
 public class ECommerceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ECommerceApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(ECommerceApplication.class, args);
+  }
 }
